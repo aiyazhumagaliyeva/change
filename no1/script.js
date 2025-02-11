232a9ce694e32bbc78acbf2a7612ef7a3e926e33
aiyazhumagaliyeva <zhuma829@newschool.edu>
2025-02-11 16:26:13 -0500

    Add files via upload

---
+++ no1/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    function showPopup() {
+        let popup = document.getElementById("popup");
+        popup.style.display = "flex";
+    }
+
+    setTimeout(showPopup, 30000);
+
+    document.getElementById("yesBtn").addEventListener("click", function() {
+        window.location.href = "../yes1/index.html";
+    });
+
+    document.getElementById("noBtn").addEventListener("click", function() {
+        let popup = document.getElementById("popup");
+        popup.style.display = "none"; 
+        setTimeout(showPopup, 30000);
+    });
+});
